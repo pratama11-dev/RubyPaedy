29b77eedc060c43c4f476d16971b8d2338141d5f
Adithya Nuz Pratama Putra <adithyanuzpratamaputra@gmail.com>
2021-06-22 19:44:00 +0700

    Upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ApplicationRecord
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
+
+    private
+    def ensure_not_referenced_by_any_line_item
+        unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+        end
+    end
+
     validates :title, :description, :image_url, presence: true
     validates :title, uniqueness: true
     validates :image_url, allow_blank: true, format: {
