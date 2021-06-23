fc942d219281d6989a43a0a6b978f5d46ee9c7e1
Adithya Nuz Pratama Putra <adithyanuzpratamaputra@gmail.com>
2021-06-23 17:52:36 +0700

    Task 15

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  skip_before_action :authorize, only: [:new, :create]
   
   include CurrentCart
     before_action :set_cart, only: [:new, :create]
@@ -32,7 +33,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        @order.charge!(pay_type_params) # do not do this
+        ChargeOrderJob.perform_later(@order,pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice:
           'Thank you for your order.' }
         format.json { render :show, status: :created,
